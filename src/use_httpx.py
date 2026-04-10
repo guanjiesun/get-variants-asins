c7f5330d1e5946be4e76a443bb170654976a2e22
guanjie <gjsun255@163.com>
2026-04-10 18:00:06 +0800

    为协程函数设置 asin 和 dst_file 参数

--- src/use_httpx.py
+++ src/use_httpx.py
@@ -4,16 +4,17 @@
 """
 
 import asyncio
+from pathlib import Path
 
 import httpx
 import pandas as pd
 
 from .get_asins import get_asins
 
-async def get_asins_by_httpx():
+async def get_asins_by_httpx(asin: str, dst_folder: Path) -> None:
     # 设置输入参数
-    asin = 'B0BLYSGFRW'
-    url = f'https://www.amazon.com/dp/{asin}'
+    url: str = f'https://www.amazon.com/dp/{asin}'
+    dst_file: Path = dst_folder / f'{asin}-httpx.xlsx'
 
     # 构造请求头
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:149.0) Gecko/20100101 Firefox/149.0"}
@@ -27,7 +28,7 @@ async def get_asins_by_httpx():
 
         # 将结果写入 excel 文件
         df = pd.DataFrame(result)
-        df.to_excel(f'{asin}-zendriver.xlsx', index=False, engine='openpyxl')
+        df.to_excel(dst_file, index=False, engine='openpyxl')
 
 if __name__ == '__main__':
-    asyncio.run(get_asins_by_httpx())
+    asyncio.run(get_asins_by_httpx(asin='B0BLYSGFRW', dst_folder=Path().cwd()))
--- src/use_zendriver.py
+++ src/use_zendriver.py
@@ -4,19 +4,22 @@
 """
 
 import asyncio
+from pathlib import Path
 
 import zendriver
 import pandas as pd
 
 from .get_asins import get_asins
 
-async def get_asins_by_zendriver():
-    asin = 'B0BLYSGFRW'
-    url = f'https://www.amazon.com/dp/{asin}'
+async def get_asins_by_zendriver(asin: str, dst_folder: Path) -> None:
+    # 设置输入参数
+    url: str = f'https://www.amazon.com/dp/{asin}'
+    dst_file: Path = dst_folder / f'{asin}-zendriver.xlsx'
 
+    # 启动浏览器
     browser = await zendriver.start()
 
-    # 获取主标签页
+    # 打开 asin 对应的详情页
     tab = await browser.get(url)
     await tab.maximize()
     await tab.wait_for_ready_state(until='complete', timeout=120)
@@ -29,11 +32,11 @@ async def get_asins_by_zendriver():
 
     # 将结果写入 excel 文件
     df = pd.DataFrame(result)
-    df.to_excel(f'{asin}-zendriver.xlsx', index=False, engine='openpyxl')
+    df.to_excel(dst_file, index=False, engine='openpyxl')
 
     # 关闭浏览器
     await asyncio.sleep(3)
     await browser.stop()
 
 if __name__ == '__main__':
-    asyncio.run(get_asins_by_zendriver())
+    asyncio.run(get_asins_by_zendriver(asin='B0BLYSGFRW', dst_folder=Path().cwd()))
